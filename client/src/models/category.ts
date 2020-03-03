b3439d7894ae47be94710a3199f3f43985b2404e
马琪斌 <maqi1520@163.com>
2020-03-03 21:48:52 +0800

    hidden

--- client/src/models/category.ts
+++ client/src/models/category.ts
@@ -3,7 +3,7 @@ import { createModel } from "@rematch/core";
 export default createModel({
   name: "category",
   state: {
-    show: new Date().getTime() > new Date("2020-03-03 12:00").getTime(),
+    show: new Date().getTime() > new Date("2020-03-04 12:00").getTime(),
     data: [],
     visible: false
   },
